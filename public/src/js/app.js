2fefb18309fe17f47ea1796e75c08724504ab43c
Lopes, Andre <andre.lopes@exxonmobil.com>
2017-11-20 16:42:14 -0200

    writting promises and using fetch api

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -4,6 +4,9 @@ if('serviceWorker' in navigator) {
         .register('/sw.js')
         .then(function() {
             console.info('Service Worker registered');
+        })
+        .catch(function(err) {
+            console.log(err);
         });
 }
 
@@ -13,3 +16,65 @@ window.addEventListener('beforeinstallprompt', function(event) {
     deferredPrompt = event;
     return false;
 });
+
+var promise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve('This is executed once the timer is done!');
+        // reject({code: 500, message: 'This is an error'})
+    }, 3000);
+});
+
+fetch('https://httpbin.org/ip')
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+
+fetch('https://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+        message: 'Does this work?'
+    }),
+    mode: 'cors'
+})
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+
+// promise.then(function(text) {
+//     return text;
+// }, function(err){
+//     console.log(err);
+// })
+//  .then(function(newText) {
+//      console.log(newText);
+// });
+
+promise.then(function(text) {
+    return text;
+})
+.then(function(newText) {
+     console.log(newText);
+})
+.catch(function(error){
+    console.log(error);
+});
+
+console.log('This is executed right after setTimeout()');
